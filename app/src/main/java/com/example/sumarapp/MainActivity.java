74f5482c941a77416e7b61f114e089c05f15a693
luispipe <lflondonor@unal.edu.co>
2023-08-03 21:36:41 -0300

    commit

--- app/src/main/java/com/example/sumarapp/MainActivity.java
+++ app/src/main/java/com/example/sumarapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.sumarapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText numero= findViewById(R.id.editTextTextValue);
+        TextView resultado= findViewById(R.id.textViewSuma);
+        Button sumar= findViewById(R.id.buttonSuma);
+
+        sumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //isEmpty es un metodo de java que valida si un elemento esta vacio
+                if(numero.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),
+                "Por favor ingrese un valor",Toast.LENGTH_LONG).show();
+                }else{
+                    if (resultado.getText().toString().isEmpty()){
+                        resultado.setText(numero.getText().toString());
+                        numero.setText("");
+                    }else{
+                        //En java los valores númericos se definen con int
+                        //Integer.parseInt es una funcionalidad a la que le entra
+                        //un texto y lo convierte en número
+                        int sumar= Integer.parseInt(resultado.getText().toString());
+                        int result= Integer.parseInt(numero.getText().toString());
+                        int total= sumar+result;
+                        //para convertir cualquier elemento es texto se usa +""
+                        // o usar String.valueOf(valorAconvertir)
+                        resultado.setText(String.valueOf(total));
+                        numero.setText("");
+                    }
+                }
+            }
+        });
+
+
+
     }
 }
