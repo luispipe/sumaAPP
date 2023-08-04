a1736519237b3ef79c521b8dbfd2a58bec971c93
luispipe <lflondonor@unal.edu.co>
2023-08-03 22:11:29 -0300

    commit

--- app/src/main/java/com/example/sumarapp/MainActivity.java
+++ app/src/main/java/com/example/sumarapp/MainActivity.java
@@ -19,6 +19,34 @@ public class MainActivity extends AppCompatActivity {
         EditText numero= findViewById(R.id.editTextTextValue);
         TextView resultado= findViewById(R.id.textViewSuma);
         Button sumar= findViewById(R.id.buttonSuma);
+        Button resta= findViewById(R.id.buttonRestar);
+
+        resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //isEmpty es un metodo de java que valida si un elemento esta vacio
+                if(numero.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),
+                            "Por favor ingrese un valor",Toast.LENGTH_LONG).show();
+                }else{
+                    if (resultado.getText().toString().isEmpty()){
+                        resultado.setText("-"+numero.getText().toString());
+                        numero.setText("");
+                    }else{
+                        //En java los valores númericos se definen con int
+                        //Integer.parseInt es una funcionalidad a la que le entra
+                        //un texto y lo convierte en número
+                        int result= Integer.parseInt(resultado.getText().toString());
+                        int restar = Integer.parseInt(numero.getText().toString());
+                        int total= result-restar;
+                        //para convertir cualquier elemento es texto se usa +""
+                        // o usar String.valueOf(valorAconvertir)
+                        resultado.setText(String.valueOf(total));
+                        numero.setText("");
+                    }
+                }
+            }
+        });
 
         sumar.setOnClickListener(new View.OnClickListener() {
             @Override
